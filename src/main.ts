c0d816768f791abdb4cdb89c447048a786f82bf2
antonin.p <antonin.p@lahautesociete.com>
2026-06-08 14:35:46 +0200

    fix: check error status to logout only on 4O1

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,23 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import {createApp} from 'vue'
+import {createPinia} from 'pinia'
 import App from './App.vue'
 import router from './router'
 import './assets/css/tokens.css'
 import './assets/css/modules.css'
 import {useAuthStore} from "@/stores/auth.ts";
+import axios from "axios";
 
 const app = createApp(App)
 app.use(createPinia())
 
 const authStore = useAuthStore()
-if(authStore.token) {
-    await authStore.fetchCurrentUser().catch(() => authStore.logout())
+if (authStore.token) {
+    await authStore.fetchCurrentUser().catch((err) => {
+        // check error status to only reset on axios error
+        if (axios.isAxiosError(err) && err.response?.status === 401) {
+            authStore.logout()
+        }
+    })
 }
 
 app.use(router).mount('#app')
